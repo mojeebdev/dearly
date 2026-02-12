53bcc200e06cb6be3301090992131d015c903114
oxmojeeb <mojeeb.eth@gmail.com>
2026-02-12 19:59:18 +0100

    upload route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { nanoid } from "nanoid";
-import { supabase } from "../../../lib/supabase";
+import { supabase } from "@/lib/supabase";
 
 export async function POST(req: NextRequest) {
   try {
